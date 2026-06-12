2b64945e8cfda3c3d77940c0b0d03c99c4e8b467
abhinaa15 <abhinanandhu79@gmail.com>
2026-06-12 09:57:12 +0530

    initial commit with python basics

---
+++ python_basic.py
@@ -0,0 +1,16 @@
+
+name=input("enter your name:")
+age=int(input("enter your age:"))
+print(f"my name is {name} iam {age} years old")
+      
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print(a+b)
+
+
+d=[13,5,6]
+d=d+["python"]
+print(d)
+
+
+print("hello"*3)
---
+++ typecast.py
@@ -0,0 +1,11 @@
+a=int("23")
+print(type(a))
+
+n=str(54)
+print(type(n))
+
+language="python"
+print(set(language))
+
+
+
