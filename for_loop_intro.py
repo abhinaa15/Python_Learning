8231dcb412c88e36afa0cbe21548ad7c9e40539e
abhinaa15 <abhinanandhu79@gmail.com>
2026-06-15 12:12:54 +0530

    first commit

---
+++ for_loop_intro.py
@@ -0,0 +1,59 @@
+"""
+for j in "python":
+    print(j)
+"""
+"""
+for k in (10,20,30,40):
+    print(k)
+"""    
+"""
+for k in range(7,0,-2):
+    print(k)
+"""
+
+"""""
+for j in range(0,10):
+    print(j)
+"""
+
+"""
+num=int(input("enter the number:"))
+fact=1
+for n in range(1,num+1):
+    fact=fact*n
+print(f"factorial of {num} is {fact}")
+"""
+"""
+l=[23,12,5,9]
+s=0
+for k in l:
+    s+=k
+print(s)   
+"""
+
+
+"""
+numbers=[45,12,23,4,8]
+for i in numbers:
+    if i%2==0:
+       print(i)
+"""
+
+"""
+count=int(input("Enter how many number you wish to enter:"))
+numbers=[]
+for k in range(1,count+1):
+    num=int(input(f"{k},Enter the number :"))
+    numbers=numbers+[num]
+print(numbers)    
+"""
+
+num=int(input("Enter the number :"))
+numbers=[]
+for k in range(2,7,8,10,4):
+    if k%2==0:
+        print(k)
+
+    
+
+ 
