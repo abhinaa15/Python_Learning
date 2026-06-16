40b7d4dc4f8e23398eb08578298293aaf89a932e
abhinaa15 <abhinanandhu79@gmail.com>
2026-06-16 11:24:09 +0530

    slicing

---
+++ slicing.py
@@ -0,0 +1,11 @@
+'''
+numbers=[10,20,30,40,50,60,70]
+print(numbers)
+'''
+'''
+numbers=list(range(1,8))
+print(numbers)
+'''
+
+numbers=[1,"John",2,"Kochi",3,4,"Python",5,6,7]
+print(numbers[::-1])
--- while_loop.py
+++ while_loop.py
@@ -4,12 +4,14 @@ while count<=10:
     print(count,end=" ")
     count+=1
 '''
-
+'''
 fruits=["Apple","Banana", "orange","Mango"]
 vowels="aeiouAEIOU"
 for k in fruits:
     if k[0] in vowels:
         print(k)
+'''
+
 
 
 
