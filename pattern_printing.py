e9ceed7c6d270535cb6862e1c9fc6b2056e668fb
abhinaa15 <abhinanandhu79@gmail.com>
2026-06-17 11:45:48 +0530

    Pattern printing basics

---
+++ pattern_printing.py
@@ -0,0 +1,13 @@
+'''
+for r in range(5):
+    for st in range(r+1):
+        print("*",end=" ")
+    print(" ")
+'''
+'''
+for r in range(1,6):
+    print("* "*r)
+'''
+
+for r in range(4,-1,-1):
+    print(" "*r,"*"*(5-r))
---
+++ practical_qstn.py
@@ -0,0 +1,38 @@
+'''
+character count from string
+
+s=input("Enter your string:")
+d={}
+
+for k in s:
+    if k in d:
+        d[k]=d[k]+1
+    else:
+        d[k]=1
+for n in d:
+    print(f"{n}={d[n]}")
+'''
+'''
+numbers=[19,2,1,23,8]
+n=len(numbers)
+for k in range(n-1):
+    for v in range(n-k-1):
+        if numbers[v]>numbers[v+1]:
+            numbers[v],numbers[v+1]=numbers[v+1],numbers[v]
+print(numbers)
+'''
+
+count=int(input("How many numbers you wish to enter:"))
+numbers=[]
+for k in range(1,count+1):
+    num=int(input(f"{k}.Enter the number:"))
+    numbers=numbers+[num]
+n=len(numbers)
+for k in range(n-1):
+    for v in range(n-k-1):
+        if numbers[v]>numbers[v+1]:
+            numbers[v],numbers[v+1]=numbers[v+1],numbers[v]
+print(numbers)
+    
+    
+
